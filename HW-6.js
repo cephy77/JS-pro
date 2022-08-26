97d0d40fca0c3952c07656e7a8855e832b3ee4bb
Oleksii Lysenko <nano77@ukr.net>
2022-08-26 14:32:56 +0300

    Homework #6

--- HW-6.js
+++ HW-6.js
@@ -13,18 +13,18 @@ function winter() {
 }
 winter();
 //The value of rabbitColor in console would be the same as inside function
-//but if concole.log was written after function - the walue would be that were written before function
+//but if concole.log was written after function - the walue would be the one that were written before function
 
 // Task 3
 //variable a = 10 in concole because:
 //function "foo" didnt found variable in inner lexical environment,
-//it get further and found "a" in outer lexical environment where varible was defined.
+//it get further and found "a" in outer lexical environment where varible was declared.
 //if there was no daclered variable there, "foo" would find "a" inside function "bar"
 
 // Task 4
 // variable a = 20 in concole because:
 //function "foo" found variable "a" in inner lexical environment then stopped search and logged out "a"
-//if inside "foo" was no "a", it would find "a" in outer lex.env. that is equal 30. 
+//if inside "foo" was no "a", it would find "a" in outer lex.env. that is equal 30.
 //If function was called before a = 30, "foo" would find "a" where "a" was declared.
 
 // Task 5
