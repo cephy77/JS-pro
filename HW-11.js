d044da0ddce2b6f2c9a287310ca18c676201a1bd
Oleksii Lysenko <nano77@ukr.net>
2022-09-16 17:58:41 +0300

    Homework #11

---
+++ HW-11.js
@@ -0,0 +1,55 @@
+// Task 1
+function logUserWithDelay(user, timeStr) {
+  const delay = timeStr.split(" ");
+  const transferToMs = (delay) => {
+    const timeUnits = {
+      second: 1000,
+      minute: 60 * 1000,
+      hour: 60 * 60 * 1000,
+    };
+    let ms = 0;
+    for (let i = 0; i < delay.length; i++) {
+      const multiplier = delay[i];
+      const timeUnitStr = delay[++i];
+      ms +=
+        multiplier *
+        (timeUnits[timeUnitStr]
+          ? timeUnits[timeUnitStr]
+          : timeUnits[timeUnitStr.slice(0, -1)]);
+    }
+    return ms;
+  };
+  return setTimeout(() => console.log(user), transferToMs(delay));
+}
+console.log(logUserWithDelay("Patrick Bateman", "1 second 4 seconds"));
+
+// Task 2
+const swords = {
+  steel: "for humans",
+  silver: "for monsters",
+};
+const scabbard = JSON.stringify(swords);
+function hideOrShowSwords(elem) {
+  if (typeof elem === "object") {
+    return JSON.stringify(elem);
+  } else {
+    try {
+      return JSON.parse(elem);
+    } catch (error) {
+      console.log("It doesn't seem to be JSON, does it?");
+    }
+  }
+}
+const hide = hideOrShowSwords(swords);
+const show = hideOrShowSwords(scabbard);
+console.log(typeof hide, typeof show);
+// Task 3
+function isItJSON(string) {
+  try {
+    JSON.parse(string);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+console.log(isItJSON(scabbard));
