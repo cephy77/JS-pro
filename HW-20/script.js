0f74956a44807e713be2302ffe4fdefd53e551d5
Oleksii Lysenko <nano77@ukr.net>
2022-10-21 17:58:25 +0300

    Homework #20

---
+++ HW-20/script.js
@@ -0,0 +1,57 @@
+const square = document.querySelector(".square");
+function centerSquare() {
+  square.style.cssText = `left: ${
+    document.documentElement.scrollWidth / 2 - square.offsetWidth / 2
+  }px;top:${
+    document.documentElement.scrollHeight / 2 - square.offsetWidth / 2
+  }px;`;
+}
+centerSquare();
+
+square.addEventListener("dblclick", centerSquare);
+square.addEventListener("pointerdown", (event) => {
+  window.addEventListener(
+    "touchmove",
+    (e) => {
+      e.preventDefault();
+    },
+    { passive: false }
+  );
+  square.classList.add("hold");
+  let shiftX = event.clientX - square.getBoundingClientRect().left;
+  let shiftY = event.clientY - square.getBoundingClientRect().top;
+  function moveSquare(pageX, pageY) {
+    function calcBorders(vector) {
+      let page, shift, length;
+      if (vector === "x") {
+        page = pageX;
+        shift = shiftX;
+        length = window.innerWidth;
+      } else {
+        page = pageY;
+        shift = shiftY;
+        length = window.innerHeight;
+      }
+      length -= square.offsetWidth;
+      if (page - shift < 0) {
+        return "0px";
+      } else if (page - shift > length) {
+        return `${length}px`;
+      } else {
+        return `${page - shift}px`;
+      }
+    }
+    let left = calcBorders("x");
+    let right = calcBorders("y");
+    square.style.left = left;
+    square.style.top = right;
+  }
+  function onPointerMove(event) {
+    moveSquare(event.pageX, event.pageY);
+  }
+  document.addEventListener("pointermove", onPointerMove);
+  square.addEventListener("pointerup", () => {
+    document.removeEventListener("pointermove", onPointerMove);
+    square.classList.remove("hold");
+  });
+});
