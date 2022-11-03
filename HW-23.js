3df8fafae11528741188b9f6897f3be3356bb19c
Oleksii Lysenko <nano77@ukr.net>
2022-11-03 17:43:22 +0200

    Homework #23

--- HW-23.js
+++ HW-23.js
@@ -1,7 +1,8 @@
 const phoneNumber = "+380674206942";
 const grossHTML = `<div>Hello</div><span>world</world>`;
 const regPhoneCheck = new RegExp(/^[+380][\d]{12}$/);
-const regTextFromHTML = new RegExp(/(?<=<\w+\d?>).+?(?=<\/.+>)/gm);
+// const regTextFromHTML = new RegExp(/(?<=<\w+\d?>).+?(?=<\/.+>)/gm);
+const regTextFromHTML = new RegExp(/>(\w+?)</);
 console.log(regPhoneCheck.test(phoneNumber));
-const textFromHtml = grossHTML.match(regTextFromHTML).join(" ");
+const textFromHtml = grossHTML.match(regTextFromHTML[1]).join(" ");
 console.log(textFromHtml);
