4daf62628112058143ec21137c712946b0362cd7
Oleksii Lysenko <nano77@ukr.net>
2022-11-08 18:40:19 +0200

    Homework #24

--- HW-24/script.js
+++ HW-24/script.js
@@ -1,19 +1,14 @@
-const express = require("express");
+import express from "express";
+import fetch from "node-fetch";
 const app = express();
 const port = 4000;
 
-app.route("/users").get((req, res) => {
-  const usersArr = [
-    "Mr. White",
-    "Mr. Orange",
-    "Mr. Blonde",
-    "Mr. Pink",
-    "Mr. Blue",
-    "Mr. Brown",
-  ];
-  res.send(usersArr);
+app.route("/users").get(async (req, res) => {
+  const response = await fetch("https://swapi.dev/api/people");
+  const data = await response.json();
+  const usersArr = Array.from(await data.results).map((user) => user.name);
+  res.send(await usersArr.join("</br>"));
 });
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
