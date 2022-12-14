da3bcd676eb4de9f3f4a3e4d5e152232dd9a0d09
Oleksii Lysenko <nano77@ukr.net>
2022-12-14 21:23:27 +0200

    Homework #31

--- hw-31/src/components/Main/Shop/ProductList/getUsers.js
+++ hw-31/src/components/Main/Shop/ProductList/getUsers.js
@@ -1,6 +1,6 @@
 function getUsers() {
   return fetch("http://localhost:4000/users")
     .then((resp) => resp.json())
-    .then((rawData) => rawData.results);
+    .then((rawData) => rawData);
 }
 export default getUsers;
