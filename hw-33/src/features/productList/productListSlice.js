de98c0e726fa338c3b5356f508774b2fddcbae80
Oleksii Lysenko <nano77@ukr.net>
2022-12-20 20:34:37 +0200

    Homework #33

--- hw-33/src/features/productList/productListSlice.js
+++ hw-33/src/features/productList/productListSlice.js
@@ -4,6 +4,7 @@ const initialState = {
   loading: false,
   products: [],
   error: false,
+  sorted: false,
 };
 
 export const getUsers = createAsyncThunk("productList/getUsers", () => {
@@ -15,6 +16,40 @@ export const getUsers = createAsyncThunk("productList/getUsers", () => {
 export const productListSlice = createSlice({
   name: "productList",
   initialState,
+  reducers: {
+    sort: (state) => {
+      if (!state.sorted) {
+        const sortedProd = state.products.map((elm) => elm);
+        sortedProd.sort((a, b) => {
+          const nameA = a.name.toUpperCase();
+          const nameB = b.name.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+        state.products = sortedProd;
+      } else {
+        const unsortedProd = state.products.map((elm) => elm);
+        unsortedProd.sort((a, b) => {
+          const idA = a.id;
+          const idB = b.id;
+          if (idA < idB) {
+            return -1;
+          }
+          if (idA > idB) {
+            return 1;
+          }
+          return 0;
+        });
+        state.products = unsortedProd;
+      }
+      state.sorted = !state.sorted;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(getUsers.pending, (state) => {
       state.loading = true;
@@ -23,15 +58,17 @@ export const productListSlice = createSlice({
       state.loading = false;
       state.products = action.payload;
       state.error = false;
+      state.sorted = false;
     });
     builder.addCase(getUsers.rejected, (state) => {
       state.loading = false;
       state.products = [];
       state.error = true;
+      state.sorted = false;
     });
   },
 });
 
-// export const { fetchUsers, incrementByAmount } = productListSlice.actions;
+export const { sort } = productListSlice.actions;
 
 export default productListSlice.reducer;
--- hw-33/src/pages/Main/Shop/ProductList/ProductList.js
+++ hw-33/src/pages/Main/Shop/ProductList/ProductList.js
@@ -1,51 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getUsers } from "../../../../features/productList/productListSlice";
+import {
+  getUsers,
+  sort,
+} from "../../../../features/productList/productListSlice";
 import style from "./ProductList.module.css";
 import Product from "./Product";
 import Loader from "../../../../helpers/Loader";
-// import getUsers from "./getUsers";
 
 function ProductList(props) {
-  // const [products, setProducts] = useState({ products: [] });
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(false);
-  // const [sorted, setSorted] = useState(false);
   const products = useSelector((state) => state.productList);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // setTimeout(() => {
-    //   getUsers()
-    //     .then((data) => setProducts({ products: data }))
-    //     .catch((error) => {
-    //       console.log(error);
-    //       setError(true);
-    //     })
-    //     .finally(() => setLoading(false));
-    // }, 2000);
     dispatch(getUsers());
   }, []);
-  // function sortProducts() {
-  //   setSorted(!sorted);
-  //   if (!sorted) {
-  //     const sortedProd = products.products.map((elm) => elm);
-  //     sortedProd.sort((a, b) => {
-  //       const nameA = a.name.toUpperCase();
-  //       const nameB = b.name.toUpperCase();
-  //       if (nameA < nameB) {
-  //         return -1;
-  //       }
-  //       if (nameA > nameB) {
-  //         return 1;
-  //       }
-  //       return 0;
-  //     });
-  //     setProducts({ products: sortedProd, unsortedProd: products.products });
-  //   } else {
-  //     setProducts({ products: products.unsortedProd });
-  //   }
-  // }
 
   if (products.loading) {
     return <Loader />;
@@ -58,15 +27,15 @@ function ProductList(props) {
   }
   return (
     <div className={style.list}>
-      {/* {// This is sorting button bellow} */}
-      {/* <button
+      <button
         className={style.sort_btn}
         onClick={() => {
-          sortProducts();
+          dispatch(sort());
         }}
       >
-        {!sorted ? <>A to Z sort</> : <>ID sort</>}
-      </button> */}
+        {!products.sorted ? <>A to Z sort</> : <>ID sort</>}
+      </button>
+
       {products.products.map((product) => (
         <Product
           name={product.name}
